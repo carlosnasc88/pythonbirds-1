eb8662dde0e79dfd03c0c31c47322284cfc0da40
carlosnasc88 <nasc1010*10>
2020-10-29 21:05:09 -0300

    criado atribudto de instancia nome e idade

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -1,11 +1,16 @@
-
-
 class Pessoa:
+    def __init__(self,nome=None, idade=32):
+        self.idade = idade
+        self.nome = nome
+
+
+
     def cumprimentar(self):
-        return 'Ola'
+        return f'Ola{id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Carlos')
     print(Pessoa.cumprimentar(p))
-    print(id(p))
+    print(id(p.nome))
+    p.nome = 'Alberto'
     print(p.cumprimentar())
