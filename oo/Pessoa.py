06736e2368dde6de7a74577df627eabaa36fc4b4
carlosnasc88 <nasc1010*10>
2020-11-03 20:49:45 -0300

    Criado e removido atributo dinâmico  de objeto Pessoa

--- oo/Pessoa.py
+++ oo/Pessoa.py
@@ -19,3 +19,7 @@ if __name__ == '__main__':
     for filho in luciano.filhos:
         print(filho.nome)
     print(luciano.filhos)
+    luciano.sobrenome = 'Nascimento'
+    del luciano.filhos
+    print(luciano.__dict__)
+    print(ailton.__dict__)
